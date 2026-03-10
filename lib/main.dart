2e8e1266a2c65e37387f2e60f96b4602d57a6ee8
sandalu <sandaluthushan20@gmail.com>
2026-03-10 13:50:05 +0530

    data base done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/main_screen.dart';
-
 import 'package:flutter/foundation.dart';
 
 void main() async {
